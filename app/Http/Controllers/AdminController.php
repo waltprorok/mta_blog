2bd29b225770d935e2b446b575c895f1f46f071d
Walter Prorok <waltprorok@gmail.com>
2019-05-24 06:55:39 -0400

    added admin middleware for admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('checkRole:admin');
+    }
+
     public function dashboard()
     {
         return view('admin.dashboard');
