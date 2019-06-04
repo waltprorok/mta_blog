3dcfba755ddd1d69bc38200cc6b88f9c391ba14f
Walter Prorok <waltprorok@gmail.com>
2019-06-04 06:53:13 -0400

    updated navigation bar a hrefs

--- resources/views/includes/admin/sidebarNavigation.blade.php
+++ resources/views/includes/admin/sidebarNavigation.blade.php
@@ -4,13 +4,13 @@
             <li class="nav-title">User</li>
 
             <li class="nav-item">
-                <a href="index.html" class="nav-link active">
+                <a href="{{ route('userDashboard') }}" class="nav-link active">
                     <i class="icon icon-speedometer"></i> Dashboard
                 </a>
             </li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('userComments') }}" class="nav-link">
                     <i class="icon icon-book-open"></i> Comments
                 </a>
             </li>
@@ -18,19 +18,19 @@
             <li class="nav-title">Author</li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('authorDashboard') }}" class="nav-link">
                     <i class="icon icon-speedometer"></i> Dashboard
                 </a>
             </li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('authorPosts') }}" class="nav-link">
                     <i class="icon icon-paper-clip"></i> Posts
                 </a>
             </li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('authorComments') }}" class="nav-link">
                     <i class="icon icon-book-open"></i> Comments
                 </a>
             </li>
@@ -38,25 +38,25 @@
             <li class="nav-title">Admin</li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('adminDashboard') }}" class="nav-link">
                     <i class="icon icon-speedometer"></i> Dashboard
                 </a>
             </li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('adminPosts') }}" class="nav-link">
                     <i class="icon icon-paper-clip"></i> Posts
                 </a>
             </li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('adminComments') }}" class="nav-link">
                     <i class="icon icon-book-open"></i> Comments
                 </a>
             </li>
 
             <li class="nav-item nav-dropdown">
-                <a href="#" class="nav-link">
+                <a href="{{ route('adminUsers') }}" class="nav-link">
                     <i class="icon icon-user"></i> Users
                 </a>
             </li>
