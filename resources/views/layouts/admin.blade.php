28a7d52cc1d3e5a1329f1ed2716cd7d302c286d0
Walter Prorok <waltprorok@gmail.com>
2019-06-07 06:55:39 -0400

    WIP: user dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'MTA Blog')</title>
+    <title>MTA - @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('admin/assets/vendor/simple-line-icons/css/simple-line-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/vendor/font-awesome/css/fontawesome-all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/css/styles.css') }}">
